995fb0f54250fdba68a1b30ad18cf811d604e141
jens270996 <55832697+jens270996@users.noreply.github.com>
2020-12-03 11:13:28 +0100

    Context

--- TestProject/MeasurementControllerTest.cs
+++ TestProject/MeasurementControllerTest.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TemperatureAPI.Data;
+using TemperatureAPI.Models;
 
 namespace TestProject
 {
     public class MeasurementControllerTest
     {
+        private DbContextOptions<ApplicationContext> _options;
+        [SetUp]
+        public void Setup()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            _options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlite(connection).Options;
+           
+            using(var context=new ApplicationContext(_options))
+            {
+                context.Database.EnsureCreated();
+                context.Locations.AddRange(
+                    new Location { Name = "Odense", Latitude = 2020, Longtitude = 2020 },
+                    new Location { Name = "Aarhus", Latitude = 1010, Longtitude = 1010 });
 
+                context.Measurements.AddRange();
+            }
+
+        }
     }
 }
