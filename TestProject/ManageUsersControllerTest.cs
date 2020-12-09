cd0cb00390d743db79df56f4a145b9d63486984f
Joachim <Kroyer.joachim@gmail.com>
2020-12-09 22:53:00 +0100

    starting tests

--- TestProject/ManageUsersControllerTest.cs
+++ TestProject/ManageUsersControllerTest.cs
@@ -1,16 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using TemperatureAPI.Controllers;
+using TemperatureAPI.Data;
 
 namespace TestProject
 {
     public class ManageUsersControllerTest
     {
+        private DbContextOptions<ApplicationContext> _options;
+        
+
         [SetUp]
-        public void SetUp()
+        public void Setup()
         {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            _options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlite(connection).Options;
+
+            using (var context = new ApplicationContext(_options))
+            {
+                context.Database.EnsureCreated();
+                context.Locations.AddRange();
+                context.Measurements.AddRange();
+                context.Users.Add(new User
+                { FirstName = "Jens", LastName = "Ane", Email = "JensAne@localhost", PwHash = "MommasBoy"});
+            }
 
         }
 
         [Test]
-        public void GetUser
+        public void GetUser()
+        {
+            ManageUsersController manage = new ManageUsersController(context:);
+            List<User> = manage.GetUser();
+        }
     }
 }
